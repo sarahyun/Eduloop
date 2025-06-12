4dc7db9757f961c34f14e0ee17067c0945e8fb5c
Sarah Yun <sarahyun02@gmail.com>
2025-06-11 20:35:21 -0400

    Revert "replit fix pt 2"
    This reverts commit 0589d96a4307e9a1a4a3cf893cc7af2d0f882523.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,11 +16,6 @@ export default defineConfig({
   server: {
     port: 5000,
     host: true,
-    allowedHosts: [
-      "490db23e-7242-49e8-8d7b-c73a04de1785-00-3717szjgpm0xs.spock.replit.dev",
-      "localhost",
-      "127.0.0.1"
-    ],
     proxy: {
       "/api": {
         target: "http://localhost:8000",
